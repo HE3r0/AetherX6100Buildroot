9023cf2c13ca2799f1c6b30a7f39084afe568f68
Maciej Muzioł <he3r00@gmail.com>
2026-07-31 15:54:00 +0200

    feat: Mac6100 bootlogo and local GUI sources

--- br2_external/package/x6100-gui/x6100_gui.mk
+++ br2_external/package/x6100-gui/x6100_gui.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 X6100_GUI_VERSION = v0.23.0-rc.3
-X6100_GUI_SITE = https://github.com/gdyuldin/x6100_gui
-X6100_GUI_SITE_METHOD = git
-X6100_GUI_GIT_SUBMODULES = YES
+X6100_GUI_SITE = /home/macmuz/Projects/x6100_gui
+X6100_GUI_SITE_METHOD = local
+# X6100_GUI_GIT_SUBMODULES = YES
 X6100_GUI_LICENSE = GPLv2
 
 X6100_GUI_INSTALL_STAGING = YES
