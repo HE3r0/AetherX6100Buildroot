31c9227b3be750296c441f26cb6ee555f4c818a8
Maciej Muzioł <he3r00@gmail.com>
2026-07-31 17:59:14 +0200

    Document local GUI SITE override for MAC6100.
    Add x6100_gui.mk.local.example and a short comment in the package makefile.

--- br2_external/package/x6100-gui/x6100_gui.mk
+++ br2_external/package/x6100-gui/x6100_gui.mk
@@ -5,6 +5,8 @@
 ################################################################################
 
 X6100_GUI_VERSION = v0.23.0-rc.3
+# MAC6100 local development SITE (this workstation).
+# For a clone-friendly / upstream-style SITE, see x6100_gui.mk.local.example
 X6100_GUI_SITE = /home/macmuz/Projects/x6100_gui
 X6100_GUI_SITE_METHOD = local
 # X6100_GUI_GIT_SUBMODULES = YES
