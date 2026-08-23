1c5af1071a1e2883ae78c5b260d5c3f129646528
Maciej Muzioł <he3r00@gmail.com>
2026-08-23 13:07:12 +0200

    Establish MAC6100 baseline: RFCOMM TTY, splash, GUI v0.34.2.
    Kernel BT/RFCOMM built-in for phone NMEA; BlueZ alias XIEGUX6100; SITE
    points at test/x6100 (no BT APP button). Reference flash: sdcard25.img.

--- br2_external/package/x6100-gui/x6100_gui.mk
+++ br2_external/package/x6100-gui/x6100_gui.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-X6100_GUI_VERSION = v0.23.0-rc.3
+X6100_GUI_VERSION = v0.34.2
 # MAC6100 local development SITE (this workstation).
 # For a clone-friendly / upstream-style SITE, see x6100_gui.mk.local.example
-X6100_GUI_SITE = /home/macmuz/Projects/x6100_gui
+X6100_GUI_SITE = /home/macmuz/Projects/test/x6100
 X6100_GUI_SITE_METHOD = local
 # X6100_GUI_GIT_SUBMODULES = YES
 X6100_GUI_LICENSE = GPLv2
